f63c0d8d13c56de718f41c0f972d5b8798fe3c07
goldenBolado <cauerodriguestva@gmail.com>
2022-12-31 00:22:43 -0300

    main

---
+++ mad-libs.py
@@ -0,0 +1,10 @@
+#   String concatenation test xD
+
+adj = input(f'Adjective: ')
+verb1 = input(f'Verb: ')
+verb2 = input(f'Verb: ')
+famous_person = input('Famous person: ')
+
+print(f'''Thanks for testing :). I love programming is so {adj}
+My favourite hobby is {verb1} and {verb2}
+My favourite celebrety is {famous_person}''')
